85f44230dcf190b6de426f1f7eb57bf6ca29a9fc
Jerry Ye <jerryye@nichols-ve703-13273.apn.wlan.private.upenn.edu>
2019-12-18 01:26:53 -0500

    added Deques

---
+++ Queues/Deque.java
@@ -0,0 +1,79 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+public class Deque<Item> implements Iterable<Item>{
+
+public LinkedList<Item> data;
+public int size;
+public Deque(){
+  data = new LinkedList<Item>();
+  size = 0;
+}
+
+public boolean isEmpty(){
+  return size == 0;
+}
+
+public int size(){
+  return size;
+}
+
+public void addFirst(Item item){
+  data.addFirst(item);
+  size += 1;
+}
+
+public void addLast(Item item){
+  data.addLast(item);
+  size += 1;
+}
+
+public Item removeFirst(){
+  size -= 1;
+  return data.removeFirst();
+}
+
+public Item removeLast(){
+  size += 1;
+  return data.removeLast();
+}
+
+public Iterator<Item> iterator(){
+  return new DequeIterator<Item>();
+}
+
+public class DequeIterator<Item> implements Iterator<Item>{
+  public int cursor;
+  public DequeIterator(){
+
+  }
+  public boolean hasNext(){
+    return cursor < size;
+  }
+  public Item next(){
+    cursor += 1;
+    return (Item) data.get(cursor - 1);
+  }
+}
+public String toString(){
+  return data.toString();
+}
+
+
+public static void main(String args[]){
+  Deque deq = new Deque<Integer>();
+  deq.addFirst(1);
+  deq.addFirst(2);
+  deq.addLast(3);
+  Iterator<Integer> iter = deq.iterator();
+  while (iter.hasNext()){
+    System.out.println(iter.next());
+  }
+  System.out.println(deq);
+  deq.removeLast();
+  System.out.println(deq);
+  deq.removeFirst();
+  System.out.println(deq);
+
+}
+
+}
